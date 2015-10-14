bc9eb16e8d03f8e3119a47ab066c374761a2c370
joelha <joel.hammar@gmail.com>
2015-10-14 17:53:11 +0200

    loginmanager initiated

--- proj/__init__.py
+++ proj/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
+lm = LoginManager()
+lm.init_app(app)
 
 from home.views import mod as homeModule
 app.register_blueprint(homeModule)
