c3550fa2febe5f41ccd827f4b35aebd49576af75
joelha <joel.hammar@gmail.com>
2015-10-14 14:37:24 +0200

    bootstrap + settings detail view

--- proj/settings/views.py
+++ proj/settings/views.py
@@ -5,3 +5,7 @@ mod = Blueprint('settings', __name__)
 @mod.route('/settings')
 def settings():
 	return render_template('settings/settings.html')
+
+@mod.route('/settings-detail')
+def detail():
+	return render_template('settings/settings-detail.html')
